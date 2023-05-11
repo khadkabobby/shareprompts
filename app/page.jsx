db128f7eb62337c4a576b8c91e4471a1db513ed1
bobby khadka <khadkaasmith2059@gmail.com>
2023-05-12 00:30:32 +0545

    setup

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Dscover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promtopia is an open-source AI promting tool for modern woeld to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
+};
+
+export default Home;
